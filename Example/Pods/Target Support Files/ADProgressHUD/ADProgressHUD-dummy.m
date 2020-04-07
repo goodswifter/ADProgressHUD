7ebdeb31d46a58e336ee81c6325aa0a433ac20e9
钟爱东 <zhongaidong@jingshonline.com>
2020-04-07 09:13:47 +0800

    【修改内容】 :  删除pod文件 【reviewer】  : 钟爱东

--- Example/Pods/Target Support Files/ADProgressHUD/ADProgressHUD-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ADProgressHUD : NSObject
-@end
-@implementation PodsDummy_ADProgressHUD
-@end
--- Example/Pods/Target Support Files/Pods-ADProgressHUD_Tests/Pods-ADProgressHUD_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ADProgressHUD_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_ADProgressHUD_Tests
-@end
